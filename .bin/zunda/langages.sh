ff17b7c53bc8ecf6649c3add77692c87adb7e323
kazuhi-ra <kazuhira4126@gmail.com>
2023-12-30 00:46:44 +0900

    いらなくね？

--- .bin/zunda/langages.sh
+++ .bin/zunda/langages.sh
@@ -16,12 +16,6 @@ mkdir -p "$(anyenv root)/plugins"
 git clone https://github.com/znz/anyenv-update.git "$(anyenv root)/plugins/anyenv-update"
 anyenv update
 
-########################## brewで入れたrustup-init ##########################
-
-if [ "$(which cargo)" = "" ]; then
-  rustup-init
-fi
-
 ########################## haskell ##########################
 # if [ "$(which ghc)" = "" ]; then
 #   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
