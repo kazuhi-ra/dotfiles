3618a10e5cd34e8100a08ef03773128a9c839747
kazuhi-ra <kazuhira4126@gmail.com>
2024-01-01 02:17:51 +0900

    linux/macで共有する設定を切り出す

--- .bin/linux/link.sh
+++ .bin/linux/link.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
+# スクリプトのディレクトリを取得
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+BASE_DIR="$(dirname "$SCRIPT_DIR")"
 
-for dotfile in "${SCRIPT_DIR}"/.??*; do
-  if [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]]; then
+# 処理対象のディレクトリを指定
+SCRIPT_DIRS=("$SCRIPT_DIR" "$BASE_DIR/shared")
+
+for dir in "${SCRIPT_DIRS[@]}"; do
+  echo "Processing directory: $dir"
+
+  # ディレクトリが存在するかチェック
+  if [[ ! -d "$dir" ]]; then
+    echo "Directory not found: $dir"
     continue
   fi
 
-  if [[ -d "$dotfile" ]]; then
-    # $dotfileがディレクトリの場合の処理
-    dotfile_basename=$(basename "$dotfile")
-    target_dir="$HOME/$dotfile_basename"
+  for dotfile in "${dir}"/.??*; do
+    if [[ "$dotfile" == "${dir}/.git" ]]; then
+      continue
+    fi
 
-    # 同名のディレクトリがなければ作成
-    mkdir -p "$target_dir"
+    if [[ -d "$dotfile" ]]; then
+      dotfile_basename=$(basename "$dotfile")
+      target_dir="$HOME/$dotfile_basename"
 
-    # ディレクトリ内のファイルに対してシンボリックリンクを作成
-    for file in "$dotfile"/*; do
-      ln -fnsv "$file" "$target_dir"
-    done
-  elif [[ -f "$dotfile" ]]; then
-    # $dotfileがファイルの場合の処理
-    ln -fnsv "$dotfile" "$HOME"
-  fi
+      mkdir -p "$target_dir"
+
+      for file in "$dotfile"/*; do
+        if [[ -d "$file" ]]; then
+          ln -fnsv "$file" "$target_dir"
+        else
+          ln -fnsv "$dotfile" "$target_dir"
+        fi
+      done
+    elif [[ -f "$dotfile" ]]; then
+      ln -fnsv "$dotfile" "$HOME"
+    fi
+  done
 done
 
 ln -fnsv "$(dirname "${SCRIPT_DIR}")/.zshrc.common.zsh" "$HOME"
