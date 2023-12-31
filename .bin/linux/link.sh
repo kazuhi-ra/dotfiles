614b2c57123bf570228e26f208ef659ab39e4bfb
kazuhi-ra <kazuhira4126@gmail.com>
2024-01-01 00:43:51 +0900

    ディレクトリを掘ってlinkするように

--- .bin/linux/link.sh
+++ .bin/linux/link.sh
@@ -3,9 +3,26 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 for dotfile in "${SCRIPT_DIR}"/.??*; do
-  [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+  if [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]]; then
+    continue
+  fi
 
-  ln -fnsv "$dotfile" "$HOME"
+  if [[ -d "$dotfile" ]]; then
+    # $dotfileがディレクトリの場合の処理
+    dotfile_basename=$(basename "$dotfile")
+    target_dir="$HOME/$dotfile_basename"
+
+    # 同名のディレクトリがなければ作成
+    mkdir -p "$target_dir"
+
+    # ディレクトリ内のファイルに対してシンボリックリンクを作成
+    for file in "$dotfile"/*; do
+      ln -fnsv "$file" "$target_dir"
+    done
+  elif [[ -f "$dotfile" ]]; then
+    # $dotfileがファイルの場合の処理
+    ln -fnsv "$dotfile" "$HOME"
+  fi
 done
 
 ln -fnsv "$(dirname "${SCRIPT_DIR}")/.zshrc.common.zsh" "$HOME"
