dc9dd4d8395580f8277401d86cc7ce15d64f589e
kazuhi-ra <kazuhira4126@gmail.com>
2024-01-01 14:00:38 +0900

    xremapのため自分をinputグループに入れる

--- .bin/linux/init.sh
+++ .bin/linux/init.sh
@@ -4,3 +4,8 @@ sudo apt update
 sudo apt upgrade
 
 chsh -s "$(which zsh)"
+
+# for xremap
+sudo usermod -a -G input $USER
+echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/99-input.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
