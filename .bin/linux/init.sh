1ebca0b7c97d0a9c1a4432167af3d6ad560effc2
kazuhi-ra <kazuhira4126@gmail.com>
2024-01-04 10:53:20 +0900

    dockerを追加
    https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository

--- .bin/linux/init.sh
+++ .bin/linux/init.sh
@@ -27,6 +27,24 @@ if [ "$(which ulauncher)" = "" ]; then
   sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y
 fi
 
+if [ "$(which docker)" = "" ]; then
+  # Add Docker's official GPG key:
+  sudo apt-get install ca-certificates curl gnupg
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  # install docker packages
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
 # apt install
 echo "apt install"
 sudo apt update
