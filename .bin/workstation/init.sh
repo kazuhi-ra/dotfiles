ef7086df66b78fe8f50350b3d481b863c1ae5da6
kazuhi-ra <kazuhira4126@gmail.com>
2024-02-16 14:01:27 +0900

    docker rootless mode

--- .bin/workstation/init.sh
+++ .bin/workstation/init.sh
@@ -4,24 +4,27 @@ sudo apt update
 sudo apt upgrade
 
 if [ "$(which docker)" = "" ]; then
-  # Add Docker's official GPG key:
-  sudo apt-get install ca-certificates curl gnupg
-  sudo install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+	# Add Docker's official GPG key:
+	sudo apt-get install ca-certificates curl gnupg
+	sudo install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-  # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
+	# Add the repository to Apt sources:
+	echo \
+		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+		sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+	sudo apt-get update
 
-  # install docker packages
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	# install docker packages
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  # add user docker group
-  sudo usermod -aG docker ${USER}
+	# add user docker group
+	sudo usermod -aG docker ${USER}
+
+	# for docker rootless mode
+	curl -fsSL https://get.docker.com/rootless | sh
 fi
 
 # apt install
@@ -33,4 +36,3 @@ xargs sudo apt install -y <"${SCRIPT_DIR}"
 
 # zshに
 chsh -s "$(which zsh)"
-
