750d7b000ef5c600b135c288435681951bccf797
kazuhi-ra <kazuhira4126@gmail.com>
2024-02-18 09:57:59 +0900

    linuxマシンでもVSCodeをつかう

--- vscode/vscode_install.sh
+++ vscode/vscode_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+# VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+VSCODE_SETTING_DIR=~/.config/Code/User
 
 rm "$VSCODE_SETTING_DIR/settings.json"
 ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
@@ -11,7 +12,7 @@ ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 # install extention
 while IFS= read -r extension; do
-  code --install-extension "$extension"
+	code --install-extension "$extension"
 done <extensions
 
 code --list-extensions >extensions
